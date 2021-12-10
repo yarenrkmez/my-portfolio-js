a0dab71fa3b83fbe5cc5041a9e02fd5930989bb9
yarenrkmez <y.mavi@hotmail.com>
2021-12-10 22:23:09 +0300

    added profile image and social media icons

--- src/components/Home/Index.js
+++ src/components/Home/Index.js
@@ -3,6 +3,7 @@ import { Home, Buttons } from "../../utils/Text";
 import AboutMe from "../AboutMe/AboutMe";
 import Navbar from "../Navbar/Navbar";
 import "./Home.css";
+import profilePhoto from "../../assets/Images/profil_photo.jpg";
 
 function Index() {
   return (
@@ -11,9 +12,39 @@ function Index() {
         style={{ height: "100vh", display: "flex", flexDirection: "column" }}
       >
         <Navbar />
+        <div style={{zIndex:99999}}>
+          <link
+            href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css"
+            rel="stylesheet"
+          />
+          <nav className="social">
+            <ul>
+              <li>
+                <a href="https://twitter.com/highflyer910">
+                  Twitter <i className="fa fa-github"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/highflyer910">
+                  Github <i className="fa fa-github"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/yaren-hilal-%C3%BCrkmez-8131331a5/">
+                  Linkedin <i className="fa fa-linkedin"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/teonamushambadze">
+                  Medium <i className="fa fa-medium"></i>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
 
         <div className="headerContent">
-          <div className="profileImg"></div>
+          <img src={profilePhoto} className="profileImg"></img>
           <div>
             <span>github</span>
             <span>github</span>
